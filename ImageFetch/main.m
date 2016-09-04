a956f044a6bc345511b1b77a71b21c76cdadbca4
saud <saudwaqarmalik@gmail.com>
2016-09-04 14:30:21 +0500

    this is how you download and store data in NSData and then save it on your system option:0 shows that you want the whole file and if the download gets interrupted then it means that you will start over again.

--- ImageFetch/main.m
+++ ImageFetch/main.m
@@ -1,17 +1,29 @@
-//
-//  main.m
-//  ImageFetch
-//
-//  Created by Saud Waqar on 9/4/16.
-//  Copyright (c) 2016 Saud Waqar. All rights reserved.
-//
+@import Foundation;
 
-#import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+int main(int argc,char *argv[])
+{
+    @autoreleasepool
+    {
+        NSURL *url = [NSURL URLWithString:@"http://www.google.com/images/logos/ps_logo2.png"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        NSError *error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                             returningResponse:NULL
+                                                         error:&error];
+        
+        if (!data) {
+            NSLog(@"failed to fetch data: %@",[error localizedDescription]);
+            return 1;
+        }
+        NSLog(@"The file is %lu bytes",(unsigned long)[data length]);
+        BOOL written = [data writeToFile:@"/Users/saudwaqar/Desktop/C/newFile.png"
+                                 options:0
+                                   error:&error];
+        if (!written ) {
+            NSLog(@"write failed: %@",[error localizedDescription]);
+            return 1;
+        }
+        NSLog(@"Success");
     }
     return 0;
 }
