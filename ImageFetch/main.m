10f556292ef49217b63cc04de0e632ad86910c47
saud <saudwaqarmalik@gmail.com>
2016-09-04 13:45:17 +0500

    Initial Commit

---
+++ ImageFetch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by Saud Waqar on 9/4/16.
+//  Copyright (c) 2016 Saud Waqar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
